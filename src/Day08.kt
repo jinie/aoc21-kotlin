4754a7103ed5870af684a0e1fab5ca04917371c1
Jimmy Selgen Nielsen <jin@bec.dk>
2021-12-07 09:59:49 +0100

    Add Day 8 template

---
+++ src/Day08.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun part1(input: List<Int>): Int {
+        return input.zipWithNext().count { (x,y) -> y > x }
+    }
+
+    fun part2(input: List<Int>): Int {
+        return input.windowed(3).map { it.sum() }.zipWithNext().count { (x,y) -> y > x }
+    }
+
+    val testInput = readInput("Day08_test").map { it.trim().toInt() }
+    check(part1(testInput) == 7)
+
+    val input = readInput("Day08").map{ it.trim().toInt() }
+    println("Part 1 : "+part1(input))
+    println("Part 2 : "+part2(input))
+}
