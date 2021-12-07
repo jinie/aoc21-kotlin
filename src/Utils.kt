07dafd16c7bf2c44105adbca46f0b6e79d77332d
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2021-12-07 09:19:23 +0100

    Cleanup

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,11 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+fun transpose(matrix: List<List<Int>>): List<List<Int>> =
+    (0 until matrix[0].size).map { column ->
+        (0 until matrix.size).map { row ->
+            matrix[row][column]
+        }
+    }
