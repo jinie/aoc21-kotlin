757dbd4f8735b9f5339529fdc80f4fe16075a42a
Jimmy Selgen Nielsen <jin@bec.dk>
2021-12-07 09:58:06 +0100

    make input more concise

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,7 +1,7 @@
 import kotlin.math.absoluteValue
 
 fun main() {
-    fun part1(input: List<Int>): Int? {
+    fun part1(input: List<Int>): Int {
         return(input.minOrNull()!!..input.maxOrNull()!!).minOf { pos ->
             input.sumOf {
                 (it-pos).absoluteValue
@@ -9,7 +9,7 @@ fun main() {
         }
     }
 
-    fun part2(input: List<Int>): Int? {
+    fun part2(input: List<Int>): Int {
         return(input.minOrNull()!!..input.maxOrNull()!!).minOf { pos ->
             input.sumOf {
                 (it-pos).absoluteValue.let { it * (it+1) / 2 }
@@ -20,7 +20,7 @@ fun main() {
     val testInput = "16,1,2,0,4,2,7,1,2,14".split(",").map { it.toInt() }.sorted()
     check(part1(testInput) == 37)
 
-    val input = readInput("Day07").map { it.split(",").map { it.trim().toInt() } }.flatten()
+    val input = readInput("Day07")[0].split(",").map { it.trim().toInt() }
     println("Part 1 : " + part1(input))
     println("Part 2 : " + part2(input))
 }
