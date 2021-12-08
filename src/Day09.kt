39424dabd01f023375d16f7c50536215ddf1e023
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2021-12-08 21:03:26 +0100

    Prepare for Day 9

---
+++ src/Day09.kt
@@ -0,0 +1,19 @@
+fun main() {
+    fun part1(input: List<Int>): Int {
+        return input.zipWithNext().count { (x,y) -> y > x }
+    }
+
+    fun part2(input: List<Int>): Int {
+        return input.windowed(3).map { it.sum() }.zipWithNext().count { (x,y) -> y > x }
+    }
+
+    val testInput = readInput("Day09_test").map { it.trim().toInt() }
+    check(part1(testInput) == 7)
+
+    measureTimeMillisPrint {
+        val input = readInput("Day09").map { it.trim().toInt() }
+        println("Part 1 : " + part1(input))
+        println("Part 2 : " + part2(input))
+    }
+
+}
