1b1c296d11985ea2b6bf2d6fee40fd0a8c4f4b32
Jimmy Selgen Nielsen <jin@bec.dk>
2021-12-09 09:24:25 +0100

    Day 9 Part 1 Solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return input.zipWithNext().count { (x,y) -> y > x }
+    fun part1(input: List<List<Int>>): Int {
+        var sum = 0
+        for(i in input.indices){
+            for(j in input[i].indices){
+                var neighbours = mutableListOf<Int>()
+
+                if(j > 0) neighbours.add(input[i][j-1]) // Check left
+                if(j < input[i].size-1) neighbours.add(input[i][j+1]) // Check right
+                if(i > 0) neighbours.add(input[i-1][j]) // Check up
+                if(i < input.size-1) neighbours.add(input[i+1][j])
+
+                if(neighbours.minOf { it } > input[i][j])
+                    sum+=input[i][j]+1
+            }
+        }
+        return sum
     }
 
-    fun part2(input: List<Int>): Int {
-        return input.windowed(3).map { it.sum() }.zipWithNext().count { (x,y) -> y > x }
+    fun part2(input: List<List<Int>>): Int {
+        return 0
     }
 
-    val testInput = readInput("Day09_test").map { it.trim().toInt() }
-    check(part1(testInput) == 7)
+    val testInput = readInput("Day09_test").map { it.trim().toCharArray().map { it.toString().toInt() }}
+    check(part1(testInput) == 15)
+
 
     measureTimeMillisPrint {
-        val input = readInput("Day09").map { it.trim().toInt() }
+        val input = readInput("Day09").map { it.trim().toCharArray().map { it.toString().toInt() } }
         println("Part 1 : " + part1(input))
         println("Part 2 : " + part2(input))
     }
