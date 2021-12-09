7e5a59596a8081e4fb82d38cdc15b91d39e3b759
Jimmy Selgen Nielsen <jin@bec.dk>
2021-12-09 11:58:16 +0100

    Refactor Day 9 Solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,42 +1,30 @@
 fun main() {
     /**
-     * Part 2 Solution stolen from https://github.com/ClouddJR/AdventOfCode2021/blob/master/src/main/kotlin/com/clouddjr/advent2021/Day09.kt for further analysis
+     * Part 2 Solution partially stolen from https://github.com/ClouddJR/AdventOfCode2021/blob/master/src/main/kotlin/com/clouddjr/advent2021/Day09.kt for further analysis
      */
 
-    val heightmap = readInput("Day09").map { row -> row.map { it.digitToInt() } }
-
-    fun neighbours(rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> {
+    fun neighbours(input: List<List<Int>>, rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> {
         return arrayOf((-1 to 0), (1 to 0), (0 to -1), (0 to 1)).map { (dx, dy) -> rowIdx + dx to colIdx + dy }
-            .filter { (x, y) -> x in heightmap.indices && y in heightmap.first().indices }
+            .filter { (x, y) -> x in input.indices && y in input.first().indices }
     }
 
-    fun basin(rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> {
-        return neighbours(rowIdx, colIdx).filterNot { (x, y) -> heightmap[x][y] == 9 }
+    fun basin(input: List<List<Int>>, rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> {
+        return neighbours(input, rowIdx, colIdx).filterNot { (x, y) -> input[x][y] == 9 }
             .fold(listOf((rowIdx to colIdx))) { points, (x, y) ->
-                points + if (heightmap[x][y] - heightmap[rowIdx][colIdx] >= 1) basin(x, y) else emptyList()
+                points + if (input[x][y] - input[rowIdx][colIdx] >= 1) basin(input, x, y) else emptyList()
             }
     }
 
-    fun lowPoints(): List<Pair<Int, Int>> {
-        return heightmap.foldIndexed(emptyList()) { rowIdx, allPoints, row ->
-            row.foldIndexed(allPoints) { colIdx, points, height ->
-                neighbours(rowIdx, colIdx).all { (x, y) -> heightmap[x][y] > height }
-                    .let { isLowest -> if (isLowest) points + (rowIdx to colIdx) else points }
+    /*    fun lowPoints(): List<Pair<Int, Int>> {
+            return heightmap.foldIndexed(emptyList()) { rowIdx, allPoints, row ->
+                row.foldIndexed(allPoints) { colIdx, points, height ->
+                    neighbours(rowIdx, colIdx).all { (x, y) -> heightmap[x][y] > height }
+                        .let { isLowest -> if (isLowest) points + (rowIdx to colIdx) else points }
+                }
             }
-        }
-    }
-
-    fun solvePart1(): Int {
-        return lowPoints().sumOf { (x, y) -> heightmap[x][y] + 1 }
-    }
-
-    fun solvePart2(): Int {
-        return lowPoints().map { (rowIdx, colIdx) -> basin(rowIdx, colIdx).toSet().size }.sortedDescending().take(3)
-            .reduce { acc, i -> acc * i }
-    }
-
-    fun part1(input: List<List<Int>>): Int {
-        var sum = 0
+        }*/
+    fun lowPoints(input: List<List<Int>>): List<Pair<Int, Int>> {
+        var ret = mutableListOf<Pair<Int, Int>>()
         for (i in input.indices) {
             for (j in input[i].indices) {
                 var neighbours = mutableListOf<Int>()
@@ -46,16 +34,26 @@ fun main() {
                 if (i > 0) neighbours.add(input[i - 1][j]) // Check up
                 if (i < input.size - 1) neighbours.add(input[i + 1][j])
 
-                if (neighbours.minOf { it } > input[i][j]) sum += input[i][j] + 1
+                if (neighbours.minOf { it } > input[i][j]) ret.add(Pair(i, j))
             }
         }
-        return sum
+        return ret.toList()
+    }
+
+    fun solvePart2(input: List<List<Int>>): Int {
+        return lowPoints(input).map { (rowIdx, colIdx) -> basin(input, rowIdx, colIdx).toSet().size }.sortedDescending()
+            .take(3).reduce { acc, i -> acc * i }
+    }
+
+
+    fun part1(input: List<List<Int>>): Int {
+        return lowPoints(input).sumOf { input[it.first][it.second] + 1 }
     }
 
     measureTimeMillisPrint {
-        val input = readInput("Day09").map { it.trim().toCharArray().map { it.toString().toInt() } }
+        val input = readInput("Day09").map { row -> row.map { it.digitToInt() } }
         println("Part 1 : " + part1(input))
-        println("Part 2 : " + solvePart2())
+        println("Part 2 : " + solvePart2(input))
     }
 
 }
