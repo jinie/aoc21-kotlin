3f9533885347b3a40f6d611c1157bc3464177738
Jimmy Selgen Nielsen <jin@bec.dk>
2021-12-10 11:22:18 +0100

    Refactor Day 10 solution

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,22 +1,24 @@
 fun main() {
     var openingBraces = listOf('(', '[', '{', '<')
     var bracePairs = mapOf(')' to '(', ']' to '[', '}' to '{', '>' to '<')
+    val illegalChars = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+    val multipliers = mapOf('(' to 1, '[' to 2, '{' to 3, '<' to 4)
 
     fun checkLine(input: String): Long {
-        val illegalChars = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
         var stack = mutableListOf<Char>()
-        var err = 0L
-        for (c in input) {
+        return input.map { c ->
             if (c in openingBraces) {
                 stack.add(c)
+                0L
             } else {
                 val cl = stack.removeLast()
                 if (bracePairs[c] != cl) {
-                    err += illegalChars[c]!!
+                    illegalChars[c]!!.toLong()
+                }else {
+                    0L
                 }
             }
-        }
-        return err
+        }.reduce{acc, i -> acc+i}
     }
 
     fun part1(input: List<String>): Long {
@@ -24,22 +26,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        val multipliers = mapOf('(' to 1, '[' to 2, '{' to 3, '<' to 4)
-        val lineScores = mutableListOf<Long>()
-        for (l in input.filterNot { checkLine(it) > 0 }) {
+        val scores = input.filterNot { checkLine(it) > 0 }.map { l ->
             var stack = mutableListOf<Char>()
-            for (c in l) {
-                if (c in openingBraces)
-                    stack.add(c)
-                else {
-                    if (stack.last() == bracePairs[c])
-                        stack.removeLast()
-                }
+            l.forEach { c ->
+                if (c in openingBraces) stack.add(c)
+                else if (stack.last() == bracePairs[c]) stack.removeLast()
             }
-            var sum: Long = 0
-            lineScores.add(stack.reversed().map { multipliers[it]!!.toLong() }.reduce { acc, i -> ((acc * 5) + i) })
-        }
-        return lineScores.sorted()[lineScores.size / 2]
+            stack.reversed().map { multipliers[it]!!.toLong() }.reduce { acc, i -> ((acc * 5) + i) }
+        }.sorted()
+        return scores[scores.size / 2]
     }
 
     val testInput = readInput("Day10_test").map { it.trim() }
@@ -50,5 +45,4 @@ fun main() {
         println("Part 1 : " + part1(input))
         println("Part 2 : " + part2(input))
     }
-
 }
