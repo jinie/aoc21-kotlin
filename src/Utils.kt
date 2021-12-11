850039f1c0fe58fd76e8be7c4949e722468a6b43
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2021-12-11 21:28:28 +0100

    Add neighbours function to utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -10,7 +10,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
 /**
  *  Reads input as a comma separated list of integers and returns a List<Int>
  */
-fun readInputToIntList(name: String, sep: String=",") = File("src", "$name.txt").readLines().map { it.trim().split(sep).map { it.toInt()} }.flatten()
+fun readInputToIntList(name: String, sep: String = ",") =
+    File("src", "$name.txt").readLines().map { it.trim().split(sep).map { it.toInt() } }.flatten()
 
 /**
  * Converts string to md5 hash.
@@ -38,7 +39,7 @@ inline fun <T> measureTimeMillis(
 inline fun <T> measureTimeMillisPrint(
     function: () -> T
 ): T {
-    return measureTimeMillis( { println("Time Taken $it ms")}){
+    return measureTimeMillis({ println("Time Taken $it ms") }) {
         function.invoke()
     }
 }
@@ -53,9 +54,19 @@ fun transpose(matrix: List<List<Int>>): List<List<Int>> = (0 until matrix[0].siz
 }
 
 /**
- * Finds neighbours in a grid
+ * Finds up/down/left/right neighbours in a grid
  */
 fun neighbours(input: List<List<Int>>, rowIdx: Int, colIdx: Int): List<Pair<Int, Int>> {
     return arrayOf((-1 to 0), (1 to 0), (0 to -1), (0 to 1)).map { (dx, dy) -> rowIdx + dx to colIdx + dy }
         .filter { (x, y) -> x in input.indices && y in input.first().indices }
+}
+
+/**
+ * Find all neighbours in a grid
+ */
+private fun allNeighbours(values: List<List<Int>>, y: Int, x: Int): List<Pair<Int, Int>> {
+    return (-1..1).flatMap { dy -> (-1..1).map { dx -> dy to dx } }
+        .filterNot { (dy, dx) -> dy == 0 && dx == 0 }
+        .map { (dy, dx) -> y + dy to x + dx }
+        .filter { (y, x) -> y in values.indices && x in values.first().indices }
 }
