623f4c1f001c545ac3e060c9a0ac668ba9501db7
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2021-12-13 12:12:44 +0100

    Day 13 refactor

--- src/Day13.kt
+++ src/Day13.kt
@@ -3,12 +3,11 @@ fun main() {
         return folds.fold(grid) { pt, i ->
             val (axis, pos) = i
             when (axis) {
-                "y" ->
-                    pt
-                        .map { if(it.y > pos) Point2d(it.x, 2 * pos - it.y) else it }
-                        .toSet()
+                "y" -> pt
+                    .map { if (it.y > pos) Point2d(it.x, 2 * pos - it.y) else it }
+                    .toSet()
                 else -> pt
-                    .map { if(it.x > pos) Point2d(2 * pos - it.x, it.y) else it }
+                    .map { if (it.x > pos) Point2d(2 * pos - it.x, it.y) else it }
                     .toSet()
             }
         }
@@ -28,13 +27,12 @@ fun main() {
 
     measureTimeMillisPrint {
         val input = readInput("Day13").map { it.trim() }
-        val input1 =
-            input.asSequence().filterNot { s -> s.contains("=") }.filter { it.isNotEmpty() }.map { it.split(",") }
+        val grid = input.asSequence().filterNot { s -> s.contains("=") }.filter { it.isNotEmpty() }.map { it.split(",") }
                 .map { p -> Point2d(p.first().toInt(), p.last().toInt()) }.toSet()
-        val input2 = input.filter { s -> s.contains("=") }.map { it.split("=") }
+        val folds = input.filter { s -> s.contains("=") }.map { it.split("=") }
             .map { p -> p.first().split(" ").last() to p.last().toInt() }
-        println("Part 1 : " + fold(input1, input2.take(1)).size)
-        draw(fold(input1, input2))
+        println("Part 1 : " + fold(grid, folds.take(1)).size)
+        draw(fold(grid, folds))
     }
 
 }
