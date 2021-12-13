4762456eba20e2fd0572105ee622cafedfa7a655
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2021-12-13 12:02:27 +0100

    Day 13 refactor

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,43 +1,40 @@
-
 fun main() {
-    fun fold(grid: Set<Point2d>, folds: List<Pair<String,Int>>): Set<Point2d>{
-        return folds.fold(grid){ pt, i ->
+    fun fold(grid: Set<Point2d>, folds: List<Pair<String, Int>>): Set<Point2d> {
+        return folds.fold(grid) { pt, i ->
             val (axis, pos) = i
             when (axis) {
                 "y" ->
                     pt
-                    .filter { it.y > pos }
-                    .map { Point2d(it.x, 2 * pos - it.y) }
-                    .toSet() + pt.filter { it.y < pos }
+                        .filter { it.y > pos }
+                        .map { Point2d(it.x, 2 * pos - it.y) }
+                        .toSet() + pt.filter { it.y < pos }
                 else -> pt
                     .filter { it.x > pos }
-                    .map { Point2d(2 * pos - it.x,it.y) }
+                    .map { Point2d(2 * pos - it.x, it.y) }
                     .toSet() + pt.filter { it.x < pos }
             }
         }
     }
 
-    fun draw(input: Set<Point2d>){
-        val out = mutableListOf<String>()
-        for(y in 0 .. input.maxOf { it.y }){
-            out.add("")
-            for(x in 0 .. input.maxOf { it.x }){
-                if(Point2d(x,y) in input){
-                    out[y]+="#"
-                }else{
-                    out[y]+=" "
+    fun draw(input: Set<Point2d>) {
+        for (y in 0..input.maxOf { it.y }) {
+            for (x in 0..input.maxOf { it.x }) {
+                when (input.contains(Point2d(x, y))) {
+                    true -> print("#")
+                    false -> print(" ")
                 }
             }
-        }
-        out.forEach{
-            println(it)
+            println()
         }
     }
 
     measureTimeMillisPrint {
         val input = readInput("Day13").map { it.trim() }
-        val input1 = input.asSequence().filterNot { s -> s.contains("=") }.filter { it.isNotEmpty() }.map { it.split(",")}.map { p -> Point2d(p.first().toInt(), p.last().toInt()) }.toSet()
-        val input2  = input.filter { s -> s.contains("=") }.map { it.split("=")}.map { p -> p.first().split(" ").last() to p.last().toInt() }
+        val input1 =
+            input.asSequence().filterNot { s -> s.contains("=") }.filter { it.isNotEmpty() }.map { it.split(",") }
+                .map { p -> Point2d(p.first().toInt(), p.last().toInt()) }.toSet()
+        val input2 = input.filter { s -> s.contains("=") }.map { it.split("=") }
+            .map { p -> p.first().split(" ").last() to p.last().toInt() }
         println("Part 1 : " + fold(input1, input2.take(1)).size)
         draw(fold(input1, input2))
     }
