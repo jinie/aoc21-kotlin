523c84721700306862f09cfb6663a04c112df808
Jimmy Selgen Nielsen <jimmy.selgen@gmail.com>
2021-12-17 11:13:14 +0100

    Refactor Day 16

--- src/main/Day16.kt
+++ src/main/Day16.kt
@@ -2,9 +2,9 @@ class Day16(input: String) {
 
     private val transmission = input.map { it.digitToInt(16).toString(2).padStart(4, '0') }.joinToString("")
 
-    fun solvePart1() = parse(transmission).sumOfVersions()
+    fun solvePart1() = parse(transmission).sumOfVersions
 
-    fun solvePart2() = parse(transmission).value()
+    fun solvePart2() = parse(transmission).value
 
     private fun parse(packet: String): Packet {
         val type = packet.drop(3).take(3).toInt(2)
@@ -43,32 +43,32 @@ class Day16(input: String) {
 }
 
 sealed class Packet(val bits: String) {
-    abstract fun sumOfVersions(): Int
-    abstract fun value(): Long
+    abstract val sumOfVersions: Int
+    abstract val value: Long
 
     val type = bits.drop(3).take(3).toInt(2)
 }
 
 class Literal(bits: String) : Packet(bits) {
-    override fun sumOfVersions() = bits.take(3).toInt(2)
+    override val sumOfVersions = bits.take(3).toInt(2)
 
-    override fun value() = bits.drop(6)
+    override val value = bits.drop(6)
         .chunked(5)
         .joinToString("") { it.drop(1) }
         .toLong(2)
 }
 
 class Operator(bits: String, private val subs: List<Packet>) : Packet(bits) {
-    override fun sumOfVersions() = bits.take(3).toInt(2) + subs.sumOf { it.sumOfVersions() }
+    override val sumOfVersions = bits.take(3).toInt(2) + subs.sumOf { it.sumOfVersions }
 
-    override fun value(): Long = when (type) {
-        0 -> subs.sumOf { it.value() }
-        1 -> subs.fold(1) { acc, packet -> acc * packet.value() }
-        2 -> subs.minOf { it.value() }
-        3 -> subs.maxOf { it.value() }
-        5 -> (subs[0].value() > subs[1].value()).toLong()
-        6 -> (subs[0].value() < subs[1].value()).toLong()
-        7 -> (subs[0].value() == subs[1].value()).toLong()
+    override val value: Long = when (type) {
+        0 -> subs.sumOf { it.value }
+        1 -> subs.fold(1) { acc, packet -> acc * packet.value }
+        2 -> subs.minOf { it.value }
+        3 -> subs.maxOf { it.value }
+        5 -> (subs[0].value > subs[1].value).toLong()
+        6 -> (subs[0].value < subs[1].value).toLong()
+        7 -> (subs[0].value == subs[1].value).toLong()
         else -> error("Unsupported type $type")
     }
 
@@ -76,7 +76,6 @@ class Operator(bits: String, private val subs: List<Packet>) : Packet(bits) {
 }
 
 fun main() {
-
     measureTimeMillisPrint {
         val d = Day16(readInput("Day16").joinToString(""))
         println("Part 1 : " + d.solvePart1())
